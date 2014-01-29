d382a36beb149f482d8ee1c40de698c7812b92e3
Stephen Nusko <nuskos@uwindsor.ca>
2014-01-29 14:09:36 -0500

    added comments to 60-340 C++ assignment 1, and created a script file to show execution of 60-330 Operating systems assignment 1

--- 60-340/assignment1/bubble.cxx
+++ 60-340/assignment1/bubble.cxx
@@ -5,12 +5,14 @@
 
 using namespace std;
 
+// bubble sort all values between first to last
 template <typename FwdIter>
 void bubble_sort(FwdIter first, FwdIter last)
 {
 	bool sorted = false;
 	while (!sorted)
 	{
+		// Assume list is sorted until you find evidence otherwise.
 		sorted = true;
 		for (FwdIter lead = next(first), follow = first; next(lead) != last; lead = next(lead), follow = next(follow))
 		{
@@ -23,7 +25,7 @@ void bubble_sort(FwdIter first, FwdIter last)
 	}
 }
 
-
+// print out the inputted value.
 template <typename T>
 void print(T input)
 {
@@ -34,11 +36,14 @@ int main()
 {
 	string input;
 	list<string> words;
+	// Until an error or EOF read in a string
 	while (cin >> input)
 	{
 		words.push_back(input);
 	}
-	bubble_sort<std::list<string>::iterator>(begin(words), end(words));
+	// bubble_sort with the iterators of the current container.
+	bubble_sort<decltype(words)::iterator>(begin(words), end(words));
+	// print out all values in the container.
 	for_each(begin(words), end(words), print<string>);
 	cout << endl;
 	return 0;
--- 60-340/assignment1/quick.cxx
+++ 60-340/assignment1/quick.cxx
@@ -4,12 +4,15 @@
 #include <string>
 
 using namespace std;
+
+// print out the inputted value.
 template <typename T>
 void print(T input)
 {
 	cout << input << " ";
 }
 
+// Find the middle between iterator first and last. (assumes they will cross).
 template <typename iter>
 iter middle(iter first, iter last)
 {
@@ -24,16 +27,24 @@ iter middle(iter first, iter last)
 	return prev(slow);
 }
 
+
+// Quick sort all values between first and last. (assumes they will cross).
 template <typename Biditer>
 void quick_sort(Biditer first, Biditer last)
 {
-	Biditer pivot = middle<Biditer>(first, last);
 	if (next(first) != last && first != last)
 	{
+		// Set the middle element as the pivot.
+		Biditer pivot = middle<Biditer>(first, last);
+		// Put the pivot out of the way of the partition.
 		iter_swap(first, pivot);
+		// Parition from first + 1 to last, around the value at first.
 		Biditer bound = partition(next(first), last, [first](typename Biditer::value_type const& value) { return value < *first; });
+		// Put the pivot back to the location it should go.
 		iter_swap(first, prev(bound));
+		// quick_sort the lower half.
 		quick_sort<Biditer>(first, prev(bound));
+		// quick_sort the upper half.
 		quick_sort<Biditer>(bound, last);
 	}
 }
@@ -45,12 +56,14 @@ int main()
 {
 	string input;
 	list<string> words;
+	// Until an error or EOF read in a string
 	while (cin >> input)
 	{
 		words.push_back(input);
 	}
-	quick_sort<std::list<string>::iterator>(begin(words), end(words));
-	cout << "final value = ";
+	// bubble_sort with the iterators of the current container.
+	quick_sort<decltype(words)::iterator>(begin(words), end(words));
+	// print out all values in the container.
 	for_each(begin(words), end(words), print<string>);
 	cout << endl;
 	return 0;
